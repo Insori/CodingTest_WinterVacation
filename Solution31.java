847181b0e4c9a94a93d750cfa1fee4d43d7f7cd7
Insori <s2110@e-mirim.hs.kr>
2023-01-05 20:50:20 +0900

    0105 내 절반이 여기에 있는가? (수정)

--- Solution31.java
+++ Solution31.java
@@ -4,7 +4,7 @@ public class Solution31 {
     public int solution(int[] arr) {
         int answer = 0;
         for (int i = 0; i < arr.length; i++)
-            for (int j = 0; j < arr.length; j++)
+            for (int j = 0; j < arr.length-1; j++)
                 if (arr[i]%2 == 0 && arr[i]/2 == arr[j]) {
                     answer++;
                     continue;
