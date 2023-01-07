8bfc4a4e8224017843c23dd252459f119e417fbd
Insori <s2110@e-mirim.hs.kr>
2023-01-07 22:47:47 +0900

    0107 짝수 홀수 개수

---
+++ Solution48.java
@@ -0,0 +1,10 @@
+public class Solution48 {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+        for(int i=0; i<num_list.length; i++) {
+            if(num_list[i]%2==0) answer[0]++;
+            else if(num_list[i]%2==1) answer[1]++;
+        }
+        return answer;
+    }
+}
