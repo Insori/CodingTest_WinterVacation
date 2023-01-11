1a545aeab96a8bf7cd04821ca447c8aa42e18004
Insori <s2110@e-mirim.hs.kr>
2023-01-11 20:36:41 +0900

    0111 옷가게 할인 받기

---
+++ Solution61.java
@@ -0,0 +1,9 @@
+public class Solution61 {
+    public int solution(int price) {
+        int answer = price;
+        if(price >= 500000) answer -= (price*0.2);
+        else if(price >= 300000) answer -= (price*0.1);
+        else if(price >= 100000) answer -= (price*0.05);
+        return answer;
+    }
+}
