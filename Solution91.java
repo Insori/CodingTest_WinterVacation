891a85b003457f67b05a8e402de0ffe43c6c57f7
Insori <s2110@e-mirim.hs.kr>
2023-01-19 23:33:16 +0900

    0119 k의 개수

---
+++ Solution91.java
@@ -0,0 +1,15 @@
+public class Solution91 {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        while(i <= j) {
+            int num = i;
+            while(num > 0) {
+                if(num%10 == k)
+                    answer++;
+                num/=10;
+            }
+            i++;
+        }
+        return answer;
+    }
+}
